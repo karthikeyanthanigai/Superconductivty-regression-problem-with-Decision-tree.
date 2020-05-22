fd7b62b8c34b7d1304726494c9d01f1672aca07e
karthikeyan thanigaivelu <51513480+karthikeyanthanigai@users.noreply.github.com>
2020-05-22 18:55:10 +0530

    Add files via upload

---
+++ dec.py
@@ -0,0 +1,30 @@
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+dataset = pd.read_csv("train.csv")
+#this function will provide the descriptive statistics of the dataset.(only int value)
+dataset.describe()
+
+#determine X and y variables(this values are taken as independent variables and dependent variable)
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+#split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train,y_test = train_test_split(X, y , test_size=0.4, random_state = 0, shuffle = True )
+
+#decision tree
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(max_features='sqrt')
+regressor.fit(X_train,y_train)
+
+#predict the test data
+y_pred = regressor.predict(X_test)
+
+#r2score
+from sklearn.metrics import r2_score
+r_squared = r2_score(y_test, y_pred)
